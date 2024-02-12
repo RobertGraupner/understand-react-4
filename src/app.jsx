c35175e247786e61eef5401900dfb83e400b25b1
Robert Graupner <robert.graupnerr@gmail.com>
2024-02-12 20:23:15 +0100

    add basic components and styles

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,19 @@
-import './app.css'
+import styles from './app.module.css';
+import { Form } from './components/Form/Form';
 
 export function App() {
-
-  return (
-    <div>
-      <header>
-        <h1>Do zrobienia</h1>
-        <h2>5 zadań</h2>
-        <button>+</button>
-      </header>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<header className={styles.header}>
+				<div>
+					<h1>Do zrobienia</h1>
+					<h2>5 zadań</h2>
+				</div>
+				<button className={styles.button}>+</button>
+			</header>
+			<Form />
+		</div>
+	);
 }
+
+export default App;
