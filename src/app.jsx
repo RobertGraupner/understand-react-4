5402c8927193b906c16f1c690a6825be777ee9a6
Robert Graupner <robert.graupnerr@gmail.com>
2024-02-12 20:00:00 +0100

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import './app.css'
+
+export function App() {
+
+  return (
+    <div>
+      <header>
+        <h1>Do zrobienia</h1>
+        <h2>5 zadań</h2>
+        <button>+</button>
+      </header>
+    </div>
+  )
+}
